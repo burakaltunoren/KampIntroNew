a970ba7d04050d33b5763bd65302e8361e533495
burakaltunoren <77547087+BurakAlt@users.noreply.github.com>
2021-01-21 18:05:43 +0300

    21Ocak2021_Donguler

---
+++ Donguler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolarDun = 7.85;
+            double dolarBugun = 7.85;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Dolar düştü");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Dolar yükseldi");
+            }
+            else
+            {
+                Console.WriteLine("Değişim yok");
+            }
+
+        }
+    }
+}
--- KampIntroNew/Program.cs
+++ KampIntroNew/Program.cs
@@ -7,6 +7,8 @@ namespace KampIntroNew
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
         }
     }
 }
