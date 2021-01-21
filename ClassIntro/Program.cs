1e45c431295e41bb9d322bd3cd031e3b43b63ad1
burakaltunoren <77547087+BurakAlt@users.noreply.github.com>
2021-01-21 19:50:49 +0300

    ClassIntro

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.urunAdi = "elma";
+            urun1.urunFiyati = 20;
+            urun1.urunAciklama = "Amasya Elma";
+
+            Urun urun2 = new Urun();
+            urun2.urunAdi = "Karpuz";
+            urun2.urunFiyati = 35;
+            urun2.urunAciklama = "Diyarbakır Karpuzu";
+
+            Urun urun3 = new Urun();
+            urun3.urunAdi = "İncir";
+            urun3.urunFiyati = 15;
+            urun3.urunAciklama = "Yerli";
+
+            Urun[] urunler = new Urun[] {urun1, urun2, urun3 };
+
+            Console.WriteLine(urunler.Length);
+
+            for (int i = 0; i < urunler.Length; i++)
+            {
+                Console.WriteLine(urunler[i].urunAdi + "  " + urunler[i].urunFiyati + " " + urunler[i].urunAciklama);
+
+            }
+
+            foreach (Urun x in urunler)
+            {
+                Console.WriteLine(x.urunAdi + "   " + x.urunFiyati + "   " + x.urunAciklama);
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ ClassIntro/Urun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassIntro
+{
+    class Urun
+    {
+        public string urunAdi { get; set; }
+        public int urunFiyati { get; set; }
+        public string urunAciklama { get; set; }
+
+    }
+}
