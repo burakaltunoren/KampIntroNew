bb71167db36c90340da5132eec1b0e1d1e6be549
burakaltunoren <77547087+BurakAlt@users.noreply.github.com>
2021-01-22 20:43:12 +0300

    OOP1

---
+++ Collections/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Engin", "Murat", "Kerem", "Halil", "ben" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler = new string[5];        //bunu yaptığımızda new kull. için bellekte heap alanında yeni bir referans adresi oluştu. 5 elemanlı boş bir dizi.
+            //isimler[4] = "Ceren";           //  bu komut artık o boş diziye şunu yapar. [ , , , ,ceren]    
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            //List<string> isimler2 = new List<string>();
+            //isimler2.Add("engin");
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("ilker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+            
+
+
+
+
+
+
+        }   
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();  //101
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            //Yukarıdaki yapı aşağıdaki formatta da kurulabilirdi
+
+            Product product2 = new Product {Id = 2, CategoryId = 5, ProductName = "Kalem", UnitPrice = 35, UnitsInStock = 5 };
+
+            //PascalCase    //camelCase
+            ProductManager productManager = new ProductManager();  //102
+            productManager.Add(product1);
+
+            productManager.Update(product1);
+
+            productManager.Topla(5, 6);
+            productManager.Topla2(10, 15);
+
+            int toplamaSonucu = productManager.Topla(5, 6);
+            Console.WriteLine(toplamaSonucu*2);
+
+
+            int sayi = 100;                     //değer tip'e örnek sorusu
+            productManager.BiseyYap(sayi);
+            Console.WriteLine(sayi);
+
+
+            
+
+
+
+        }
+        
+
+        
+    }       
+}
