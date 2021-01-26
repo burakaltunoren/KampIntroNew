a914baee3c361b9dbbacd63bb45640772aa1843f
burakaltunoren <77547087+BurakAlt@users.noreply.github.com>
2021-01-26 12:57:14 +0300

    26 Ocak 2021

---
+++ OOP3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            tasitKrediManager.Hesapla();
+
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            ihtiyacKrediManager.Hesapla();
+
+            IKrediManager konutKrediManager = new KonutKrediManager();  //***Interface 'ler de onları implemente eden classların referansını tutabiliyor.***
+            konutKrediManager.Hesapla();
+
+        }
+    }
+}
