a991757c52a514f0073d400c721a81ea373d050c
burakaltunoren <77547087+BurakAlt@users.noreply.github.com>
2021-01-27 20:04:01 +0300

    27 01 2021

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)       // IKrediManager bir interface ve tanımladığımız 3 krediyi de zaten tutuyor. Bu yüzden bu 3 krd'den hangisini seçsek kredi parametresine aktarılacak.
+        {                                                                                      // ve o kredinin bellekteki Hesapla kodları çalışacak. Hatırlayalım; Interface'ler de implemente edildikleri class'ların referansını tutardı.
+            krediManager.Hesapla();
+            loggerService.Log();
+            
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager>krediler) 
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();               
+
+            }
+        
+        }
+
+    }
+}
--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP3
 {
@@ -6,15 +7,27 @@ namespace OOP3
     {
         static void Main(string[] args)
         {
-            IKrediManager tasitKrediManager = new TasitKrediManager();
-            tasitKrediManager.Hesapla();
+            IKrediManager tasitKrediManager = new TasitKrediManager();        //a
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();    //b
+            IKrediManager konutKrediManager = new KonutKrediManager();        //c            //***Interface 'ler de onları implemente eden classların referansını tutabiliyor.***
+            ILoggerService dataBaseLogger = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
 
-            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
-            ihtiyacKrediManager.Hesapla();
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager, new DatabaseLoggerService() );   //bu satır aşağıdaki ile aynı yazımdır. Yukarıda new'lemediysek bu satırdaki gibi yazarak da new 'leyebiliyoruz.                
+            basvuruManager.BasvuruYap(ihtiyacKrediManager, new DatabaseLoggerService());                    
+                                                                                                           
 
-            IKrediManager konutKrediManager = new KonutKrediManager();  //***Interface 'ler de onları implemente eden classların referansını tutabiliyor.***
-            konutKrediManager.Hesapla();
+            List<IKrediManager> krediler = new List<IKrediManager> {konutKrediManager, tasitKrediManager, ihtiyacKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+
+
+
+
+
 
         }
-    }
-}
+    }                                                                         
+}                                                                             
