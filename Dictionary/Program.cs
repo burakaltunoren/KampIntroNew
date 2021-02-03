0724bd446f796124ed391b7e505d75c51dff8d0f
burakaltunoren <77547087+BurakAlt@users.noreply.github.com>
2021-02-03 19:15:59 +0300

    Dictionary: (Tamamlanmadı)

---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> sehirPlaka = new Dictionary<int, string>();
+            sehirPlaka.Add(01, "Adana");
+            sehirPlaka.Add(33, "Mersin");
+            sehirPlaka.Add(46, "Maraş");
+            sehirPlaka.Add(27, "G.Antep");
+            sehirPlaka.Add(06, "Ankara");
+            sehirPlaka.Add(34, "İstanbul");
+
+            foreach (var deger in sehirPlaka)
+            {
+                Console.WriteLine(deger);
+            }
+
+            
+
+
+
+
+        }
+    }
+}
